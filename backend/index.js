a7bc3ba484fa9560ff4889008da55cbd1a5ec195
Michele Sciabarra <michele@sciabarra.com>
2021-10-24 20:15:41 +0200

    api

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,45 @@
 const express = require('express')
+const cors = require("cors")
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+app.use(cors())
 
 const { Client } = require('pg')
 let client = undefined
 
-let create = `
+app.get('/', (req, res) => {
+  client.query("SELECT id, message FROM guestbook ORDER BY id")
+  .then(r =>  res.send(r.rows))
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  const msg = req.body.msg
+  client.query("INSERT INTO guestbook(message) VALUES($1)", [msg])
+  .then(r => res.send({ "changed": r.rowCount }))
+})
+
+const createTable = `
 CREATE TABLE IF NOT EXISTS guestbook( 
    id SERIAL PRIMARY KEY,
    message TEXT
 )`
 
-function connectAndInitialize() {
+function connect() {
   console.log("connecting to database")
   client = new Client()
   client.connect()
     .then(() => {
-      client.query(create)
+      client.query(createTable)
       console.log("connected and initialized")
     })
     .catch((err) => {
       console.log(err)
       console.log("cannot connect, retrying")
-      setTimeout(connectAndInitialize, 2000)
+      setTimeout(connect, 2000)
     })
 }
 
-app.listen(port, connectAndInitialize)
+app.listen(port, () => connect() )
--- pres.md
+++ pres.md
@@ -395,9 +395,16 @@ databases:
 ```sh
 cp src/3-app.yaml .do/app.yaml 
 cp src/3-index.js backend/index.js
+git add backend
+git commit -m "database" -a
+git push origin main
 # update
 doctl app update $ID --spec .do/app.yaml
 ```
+
+---
+
+![bg fit](img/3-database.png)
 
 ---
 
