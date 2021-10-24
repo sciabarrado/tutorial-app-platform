ec445976024dfa35959392cba1bbf4c54364f688
Michele Sciabarra <michele@sciabarra.com>
2021-10-24 19:46:56 +0200

    frontend

--- reset.sh
+++ reset.sh
@@ -4,7 +4,8 @@ doctl app delete $ID --force
 doctl auth remove --context default
 rm -Rvf backend
 rm -Rvf frontend
-git commit -m "cleanup"
+rm -Rvf .do
+git commit -m "reset" -a 
 git push origin main
 psql -c 'drop database localdb'
 psql -c 'drop user demo'
