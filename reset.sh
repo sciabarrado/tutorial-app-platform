27334aa1de7514afbb01c60f0f270de612e124f8
Michele Sciabarra <michele@sciabarra.com>
2021-10-26 09:05:09 +0200

    reset

--- reset.sh
+++ reset.sh
@@ -1,7 +1,7 @@
 ID=$(doctl app list | awk '/tutorial-app-platform/ { print $1 }')
 echo $ID
 doctl app delete $ID --force
-doctl auth remove --context default
+#doctl auth remove --context default
 rm -Rvf backend
 rm -Rvf frontend
 rm -Rvf .do
